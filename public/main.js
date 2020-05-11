7d41ec96b745269656484610e7c0cd0f53b49596
Will Moritz <will@crowdfavorite.com>
2020-05-11 14:17:02 -0400

    initalized firebase correctly

---
+++ public/main.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADcwKzmElrr-XQAzyfHl8PukYRMaWfCcE",
+  authDomain: "walloftext-78632.firebaseapp.com",
+  databaseURL: "https://walloftext-78632.firebaseio.com",
+  projectId: "walloftext-78632",
+  storageBucket: "walloftext-78632.appspot.com",
+  messagingSenderId: "609649100590",
+  appId: "1:609649100590:web:1db85680c7ff14cc241cbf",
+  measurementId: "G-7KYF8P2111",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+console.log('yolo')
